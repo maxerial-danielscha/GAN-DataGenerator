6795da34d13428df5de32bdcc27f52ccfd3a0f59
Daniel Schafhäutle <daniel.schafhaeutle@maxerial.com>
2024-11-08 13:36:33 +0100

    dcgan base 64x64 is running but not converged

--- config.py
+++ config.py
@@ -6,3 +6,10 @@ cwd = os.getcwd()
 
 data_path = join(cwd, "data")
 
+
+# We want more of that type of image
+data_existing = join(data_path, "existing")
+
+
+# There you have more of that type of image
+data_generated = join(data_path, "generated")
